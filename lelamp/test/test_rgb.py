cef2bd64339dbd6ab87c7f4d849a37f2695de002
Binh Pham <phamtuanbinh1504@gmail.com>
2025-08-15 00:41:30 +0300

    Add servo test

--- lelamp/test/test_rgb.py
+++ lelamp/test/test_rgb.py
@@ -3,12 +3,12 @@ from rpi_ws281x import PixelStrip, Color
 
 # LED strip configuration:
 LED_COUNT = 40        # Number of LEDs in your strip
-LED_PIN = 18          # GPIO pin connected to the pixels (18 supports PWM)
+LED_PIN = 12         # GPIO pin connected to the pixels (12 supports PWM)
 LED_FREQ_HZ = 800000  # WS2812B LED signal frequency (800kHz)
 LED_DMA = 10          # DMA channel to use
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False    # True to invert the signal (if needed)
-LED_CHANNEL = 0       # 0 for GPIO 18, 1 for GPIO 13,19, etc.
+LED_CHANNEL = 0       # 0 for GPIO 12, 1 for GPIO 13,19, etc.
 
 # Create PixelStrip object
 strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
---
+++ lelamp/test/test_servo.py
@@ -0,0 +1,27 @@
+import argparse
+from ..follower import LeLampFollowerConfig, LeLampFollower
+  
+def main():
+    parser = argparse.ArgumentParser(description="Check motors status and position")
+    parser.add_argument('--id', type=str, required=True, help='ID of the lamp')
+    parser.add_argument('--port', type=str, required=True, help='Serial port for the lamp')
+    args = parser.parse_args()
+
+    robot_config = LeLampFollowerConfig(
+        port=args.port,
+        id=args.id,
+    )
+
+    robot = LeLampFollower(robot_config)
+    robot.connect(calibrate=False)
+
+    while True:
+        try:
+            obs = robot.get_observation()
+            print(obs)
+        except KeyboardInterrupt:
+            print("Shutting down teleop...")
+            break
+
+if __name__ == "__main__":
+    main()
